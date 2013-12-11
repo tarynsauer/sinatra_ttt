bc70157725deb10ffbb76714a2cfafca20607846
Taryn Sauer <taryn.sauer@gmail.com>
2013-12-11 15:53:20 -0600

    Add buttons to play page

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 
-# GET #########
+# GET ------------------------------
 get '/' do
   erb :index
 end
 
 get '/play' do
+  @board      = Board.new(3)
+  @player_one = Player.new(MARKER_X)
+  @player_two = Player.new(MARKER_O)
+  @player_one.player_type = params["player-one"]
+  @player_two.player_type = params["player-two"]
+  @game = Game.new(@board, @player_one, @player_two, params["difficulty"])
   erb :play
-end
-
-# POST #########
-put '/' do
-  # Create new game with form params
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,7 @@
 require './app'
+require 'board.rb'
+require 'ai.rb'
+require 'player.rb'
+require 'ui.rb'
+require 'game.rb'
 run Sinatra::Application
