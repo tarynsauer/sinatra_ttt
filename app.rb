b4c372b3d37edb82ce7af0667b2d1609328a6400
Taryn Sauer <taryn.sauer@gmail.com>
2013-12-11 11:48:04 -0600

    Add form to homepage

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 
+# GET #########
 get '/' do
   erb :index
+end
+
+get '/play' do
+  erb :play
+end
+
+# POST #########
+put '/' do
+  # Create new game with form params
 end
