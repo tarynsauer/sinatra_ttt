76ad2c8d9ee53ce0d022871fa47c044fbe0d2f11
Taryn Sauer <taryn.sauer@gmail.com>
2013-12-16 16:42:48 -0600

    Remove deleted files

--- app/helpers/game_helpers.rb
+++
@@ -1,7 +0,0 @@
-helpers do
-  def verify_move(game, cell)
-    if game.board.available_cell?(cell)
-      game.board.add_marker(game.current_player.marker, cell)
-    end
-  end
-end
--- app/helpers/web_ui_helpers.rb
+++
@@ -1,5 +0,0 @@
-helpers do
-  def get_game_message(game)
-    game.board.empty? ? game.ui.first_move_message(game.current_player) : game.ui.next_move_message(game.current_player)
-  end
-end
